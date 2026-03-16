44cee2f9194d0a1a2713419bd40eab1df2c9ecba
andre okanabr <alhkanashiro@gmail.com>
2026-03-16 17:48:45 -0300

    Add react

---
+++ 1_INTRO/hello-world-vite/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1>Hello World c/ vite</h1>
+			<h2>Funfa</h2>
+			<h2>Funfou</h2>
+			<h2>Funcionou</h2>
+		</>
+	);
+}
+
+export default App;
---
+++ Desafios/desafio1/src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Venus</h1>
+			<h2>Terra</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+		</div>
+	);
+}
+
+export default App;

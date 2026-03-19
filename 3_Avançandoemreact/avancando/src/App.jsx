0225dcdc1cb58580d5eae01fb78f0ca48ec143d8
andre okanabr <alhkanashiro@gmail.com>
2026-03-19 11:17:28 -0300

    UPDATE aulas

--- 3_Avançandoemreact/avancando/src/App.jsx
+++ 3_Avançandoemreact/avancando/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 
 import City from "./assets/city.jpg";
+import ConditionalRender from "./components/ConditionalRender";
+import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
+import ShowUserName from "./components/ShowUserName";
 
 function App() {
+	const name = "MJ";
 	return (
 		<div className="App">
 			<h1>Avançando em React</h1>
@@ -16,6 +20,9 @@ function App() {
 				<img src={City} alt="Cidade" />
 			</div>
 			<ManageData />
+			<ListRender />
+			<ConditionalRender />
+			<ShowUserName name={name} />
 		</div>
 	);
 }

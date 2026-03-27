4ef31bac9be56d142641e1b3985c26d32fba02ab
andre okanabr <alhkanashiro@gmail.com>
2026-03-27 09:11:03 -0300

    Desafio 9)

---
+++ 9_CONTEXT/context/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Context</h1>
+			{/* <BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="products" element={<Products />} />
+					<Route path="about" element={<About />} />
+				</Routes>
+			</BrowserRouter> */}
+		</div>
+	);
+}
+
+export default App;
---
+++ Desafios/desafio8/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Info from "./pages/Info";
+
+function App() {
+	return (
+		<div>
+			<BrowserRouter>
+				<h1>Desafio 8/ Tarefa 8 - Páginas</h1>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/info" element={<Info />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+}
+
+export default App;

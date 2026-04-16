6be41cbf71424684e1ef8d978919981b91d978d3
andre <alhkanashiro@gmail.com>
2026-04-16 11:10:51 -0300

    Navbar/Footer/Routes

---
+++ Modulo C - Aulas 10 a 13/13_REACTGRAM/frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+	return (
+		<footer id="footer">
+			<p>ReacGram &copy; {new Date().getFullYear()}</p>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ Modulo C - Aulas 10 a 13/13_REACTGRAM/frontend/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import "./Navbar.css";
+
+//Components
+import { NavLink, Link } from "react-router-dom";
+import {
+	BsSearch,
+	BsHouseDoorFill,
+	BsFillPersonFill,
+	BsFillCameraFill,
+} from "react-icons/bs";
+
+// Hooks
+import { useState } from "react";
+// import { useAuth } from "../hooks/useAuth";
+// import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+	return (
+		<nav id="nav">
+			<Link to="/">ReactGram</Link>
+			<form id="search-form">
+				<BsSearch />
+				<input type="text" />
+			</form>
+			<ul id="nav-links">
+				<NavLink to="/">
+					<BsHouseDoorFill />
+				</NavLink>
+				<NavLink to="/login">Entrar</NavLink>
+				<NavLink to="/register">Cadastrar</NavLink>
+			</ul>
+		</nav>
+	);
+};
+
+export default Navbar;
